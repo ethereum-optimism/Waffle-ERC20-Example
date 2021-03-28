9c49f09122be1a1142ab98a3ee6454144e90e3ec
platocrat <jack.abe.winfield@gmail.com>
2021-03-28 18:45:08 -0500

    update -> need accts with balances

--- test/erc20.spec.ts
+++ test/erc20.spec.ts
@@ -1,87 +1,88 @@
 /* External imports */
-import { expect } from 'chai'
+import { expect, use } from 'chai'
 import { ethers, Wallet, Signer } from 'ethers'
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { solidity, deployContract } from 'ethereum-waffle'
 import 'dotenv/config'
 
 /* Internal imports */
+import { ERC20 } from '../types'
 const buildFolder = (process.env.TEST_MODE === 'OVM') ? 'build-ovm' : 'build'
-const ERC20 = require(`../${ buildFolder }/ERC20.json`)
+const ERC20Aritfact = require(`../${ buildFolder }/ERC20.json`)
 
-// use(solidity)
+use(solidity)
 
 describe('ERC20 smart contract', () => {
-  let provider: JsonRpcProvider,
-    wallet: Wallet,
-    walletTo: Wallet
+  let ERC20: any,
+    provider: JsonRpcProvider
 
-  // // For the Optimistic test file
   const privateKey1: string = ethers.Wallet.createRandom().privateKey
   const privateKey2: string = ethers.Wallet.createRandom().privateKey
-  const privateKey3: string = ethers.Wallet.createRandom().privateKey
+  const useL2: boolean = (process.env.TEST_MODE === 'OVM')
+
+  if (useL2 == true) {
+    provider = new ethers.providers.JsonRpcProvider(process.env.L2_WEB3_URL)
+  } else {
+    provider = new ethers.providers.JsonRpcProvider(process.env.L1_WEB3_URL)
+  }
+
+  /**
+   * @dev NEED TO CREATE WALLETS WITH BALANCES COVER CONTRACT DEPLOYMENT COST
+   */
+  const wallet: Signer = new ethers.Wallet(privateKey1, provider)
+  const walletTo: Signer = new ethers.Wallet(privateKey2, provider)
 
   before(async () => {
-    if (process.env.TEST_MODE === 'OVM') {
-      provider = new ethers.providers.JsonRpcProvider(process.env.L2_WEB3_URL)
-    } else {
-      provider = new ethers.providers.JsonRpcProvider(process.env.L1_WEB3_URL)
-    }
-
-    // Signers
-    const account1: Signer = new ethers.Wallet(privateKey1, provider)
-    const account2: Signer = new ethers.Wallet(privateKey2, provider)
-    const account3: Signer = new ethers.Wallet(privateKey3, provider)
-    // const wallets = provider.getWallets()
-    // wallet = wallets[0]
-    // walletTo = wallets[1]
+    console.log('Wallet balance: ', (await wallet.getBalance()).toString())
+    console.log('WalletTo balance: ', (await walletTo.getBalance()).toString())
   })
 
   // parameters to use for our test coin
   const COIN_NAME = 'OVM Test Coin'
   const TICKER = 'OVM'
   const NUM_DECIMALS = 1
-  let token
 
-  /* Deploy a new ERC20 Token before each test */
-  beforeEach(async () => {
-    token = await deployContract(wallet, ERC20, [1000, COIN_NAME, NUM_DECIMALS, TICKER])
+  /* Deploy a new ERC20 Token  */
+  before(async () => {
+    ERC20 = await deployContract(wallet, ERC20Aritfact, [1000, COIN_NAME, NUM_DECIMALS, TICKER]) as ERC20
+
+    console.log('\n \n The deployed contract: \n', ERC20)
   })
 
   it('Assigns initial balance', async () => {
-    expect(await token.balanceOf(wallet.address)).to.equal(1000)
+    expect(await ERC20.balanceOf(wallet.getAddress())).to.equal(1000)
   })
 
   it('Correctly sets vanity information', async () => {
-    const name = await token.name()
+    const name = await ERC20.name()
     expect(name).to.equal(COIN_NAME)
 
-    const decimals = await token.decimals()
+    const decimals = await ERC20.decimals()
     expect(decimals).to.equal(NUM_DECIMALS)
 
-    const symbol = await token.symbol()
+    const symbol = await ERC20.symbol()
     expect(symbol).to.equal(TICKER)
   })
 
 
   it('Transfer adds amount to destination account', async () => {
-    await token.transfer(walletTo.address, 7)
-    expect(await token.balanceOf(walletTo.address)).to.equal(7)
+    await ERC20.transfer(walletTo.getAddress(), 7)
+    expect(await ERC20.balanceOf(walletTo.getAddress())).to.equal(7)
   })
 
   it('Transfer emits event', async () => {
-    await expect(token.transfer(walletTo.address, 7))
-      .to.emit(token, 'Transfer')
-      .withArgs(wallet.address, walletTo.address, 7)
+    await expect(ERC20.transfer(walletTo.getAddress(), 7))
+      .to.emit(ERC20, 'Transfer')
+      .withArgs(wallet.getAddress(), walletTo.getAddress(), 7)
   })
 
   it('Can not transfer above the amount', async () => {
-    await expect(token.transfer(walletTo.address, 1007)).to.be.reverted
+    await expect(ERC20.transfer(walletTo.getAddress(), 1007)).to.be.reverted
   })
 
   it('Can not transfer from empty account', async () => {
-    const tokenFromOtherWallet = token.connect(walletTo)
-    await expect(tokenFromOtherWallet.transfer(wallet.address, 1))
+    const ERC20FromOtherWallet = ERC20.connect(walletTo)
+    await expect(ERC20FromOtherWallet.transfer(wallet.getAddress(), 1))
       .to.be.reverted
   })
 })
